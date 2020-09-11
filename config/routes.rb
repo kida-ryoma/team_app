10710a59243b0942b60c756816a0342db80ee83c
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-09-11 13:59:36 +0900

    intro devise
    deviseの導入
    管理ユーザー登録ページ/機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers:{
+    registrations: "users/registrations"
+  }
+  devise_scope :user do
+    get 'users/registrations/admin_signup' => 'users/registrations#admin_new'
+    post "users/registrations/admin_create" => "users/registrations#admin_create"
+  end
   get 'homes/index'
   root "homes#index"
 end
