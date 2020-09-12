a1757e71c0b143e3cd518740f56460388aff2ab2
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-09-12 16:16:06 +0900

    UserとTeamモデルのアソシエーション記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: true
   # validates :admin, presence: true
+  belongs_to :team
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
