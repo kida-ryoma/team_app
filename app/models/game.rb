05b5dd3b36ee4793871e00e8cbd82def48f25331
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-09-15 16:38:24 +0900

    通知メソッド定義
    試合がcreateされた時に通知される（Nortificationモデルのインスタンスが生成される）メソッドをGameモデルに定義

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,14 @@ class Game < ApplicationRecord
   has_many :games_users
   has_many :users, through: :games_users
   has_many :notifications, dependent: :destroy
+
+  # ◆通知機能（新規試合案内）
+  def create_notification_new_game!(current_user, visited_id)
+    notification = current_user.active_notifications.new(
+      game_id: id,
+      visited_id: visited_id,
+      action: 'new_game'
+    )
+    # 自分へは通知が作られない・届かないようにする
+    notification.save if notification.visitor_id != notification.visited_id && notification.valid?
 end
--- app/models/nortification.rb
+++ app/models/nortification.rb
@@ -1,6 +1,6 @@
 class Nortification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
-  belongs_to :game, foreign_key: true
+  belongs_to :game, optional: true
 
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
