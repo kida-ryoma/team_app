420d91d6a41e1ba019cb0b759d1352deb5666b06
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-09-22 14:33:15 +0900

    N+1問題対策

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_team
+  before_action :set_games_users
   def show
   end
 
@@ -9,4 +10,9 @@ class UsersController < ApplicationController
   def set_team
     @team = Team.find(current_user.team_id)
   end
+
+  def set_games_users
+    @games_users = GamesUser.includes(:game).where('user_id = ? and status = ?', current_user.id, 'notyet')
+  end
+
 end
