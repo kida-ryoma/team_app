cdc00eb295291807f593fcd74f8980aa47fa58cb
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-09-22 16:01:30 +0900

    ActionMailer、本番環境様に編集

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -5,6 +5,7 @@ class RegistrationMailer < ApplicationMailer
     @team = team
     @email = email
     @url = "http://localhost:3000/teams/#{@team.id}/add_user"
+    @production_url = "http://54.199.111.49/teams/#{@team.id}/add_user"
     mail(subject: "TEAM APPへの登録のご案内" ,to: "#{@email.email}")
   end
 end
