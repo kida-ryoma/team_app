4b332d9cded15b9e1141a8ae0c544fb3a5a17a30
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-10-14 17:16:16 +0900

    メソッド追加
    simpleccalendarの仕様に合わせるためのメソッド

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,6 +11,17 @@ class Game < ApplicationRecord
   has_many :users, through: :games_users
   has_many :notifications, dependent: :destroy
 
+
+
+  # ◆カレンダー：gem simple_calendarでは"start_time"と"end_time"ベースでカレンダーのdayに入る。今回は日付と時間の入力を分けており、時間カラムには今日に日付が入ってしまうため、simple_calendarの仕様踏まえて明示的にstart_timeとend_timeを日付カラムの日にちに設定しなければならない。そのため開始時間と終了時間の命名をかぶらないようにbegin_timeとfinish_timeにした。
+  def start_time
+    date
+  end
+
+  def end_time
+    date
+  end
+
   # ◆通知機能（新規試合案内）
   def create_notification_new_game!(current_user, visited_id)
     notification = current_user.active_notifications.new(
