9a8bbedbba96b71b8a086d84ea32e13f7ec456b4
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-10-20 10:42:49 +0900

    メッセージ作成アクション追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+  def create
+    @dm = current_user.messages.new(dm_params)
+    if @dm.save
+      @dm.create_notification_dm(current_user, session[:user_id])
+      redirect_to room_path(params[:room_id])
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def dm_params
+    params.require(:message).permit(:content, :room_id)
+  end
 end
